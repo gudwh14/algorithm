6609e4c1144ca11aee22a472d429ede4943798e7
gudwh14 <alxious@naver.com>
2022-02-24 18:10:07 +0900

    samsung : 사다리조작 시간초과 해결

--- boj/samsung/사다리조작.py
+++ boj/samsung/사다리조작.py
@@ -1,4 +1,3 @@
-import copy
 import itertools
 import sys
 
@@ -12,16 +11,16 @@ def print_board(board):
 # 사다리 게임 시작하는 함수
 def start_game(n, board, hor_ladder):
     # 열을 조사
-    for ladder in range(1, n):
+    for ladder in range(1, n + 1):
         i = 0
         j = ladder
         while board[i][j] != '끝':
             # 밑으로 내려가기
             i += 1
             # 수평 사다리 만나면 이동하기
-            if [i, j] in hor_ladder:
+            if hor_ladder[i][j]:
                 j += 1
-            elif [i, j - 1] in hor_ladder:
+            elif hor_ladder[i][j - 1]:
                 j -= 1
         # 일치하지 않으면 FALSE 반환 및 종료
         if j != ladder:
@@ -29,9 +28,14 @@ def start_game(n, board, hor_ladder):
     return True
 
 
-def solution(n, m, h, hor_ladder):
+def solution(n, m, h, ladder):
     # 사다리 게임 보드 그리기
     board = [['ㅣ' for _ in range(n + 2)] for _ in range(h + 2)]
+    hor_ladder = [[False] * (n + 2) for _ in range(h + 2)]
+
+    for i, j in ladder:
+        hor_ladder[i][j] = True
+
     for i in range(h + 2):
         for j in range(n + 2):
             if i == 0:
@@ -41,27 +45,27 @@ def solution(n, m, h, hor_ladder):
             if j == 0 or j == n + 1:
                 board[i][j] = '빈'
 
-    # 수평 사다리를 설치할수 있는 유요한 좌표 저장하는 변수
+    # 수평 사다리를 설치할수 있는 유효한 좌표를 저장하는 변수
     ladders = []
     for i in range(1, h + 1):
         for j in range(1, n):
             # 현재 놓여진 수평사다리와 비교하여 동일한 사다리가 있는지, 설치하려는 사다리 좌우에 수평사다리가 있는지 바교
-            if [i, j] not in hor_ladder and [i, j - 1] not in hor_ladder and [i, j + 1] not in hor_ladder:
+            if not hor_ladder[i][j] and not hor_ladder[i][j - 1] and not hor_ladder[i][j + 1]:
                 ladders.append([i, j])
 
     # 0 ~ 3개까지 수평사다리를 설치할수있는 조합을 구하여 반복문 실행
     for count in range(0, 4):
         for comb in list(itertools.combinations(ladders, count)):
             flag = False
-            _hor_ladder = copy.deepcopy(hor_ladder)
+            _hor_ladder = [item[:] for item in hor_ladder]
             for ladder in comb:
                 i, j = ladder
                 # 추가할 수평사다리가 유요한 수평사다리인지 검사
-                if [i, j - 1] in _hor_ladder or [i, j + 1] in _hor_ladder:
+                if hor_ladder[i][j - 1] or _hor_ladder[i][j + 1]:
                     flag = True
                     break
 
-                _hor_ladder.append(ladder)
+                _hor_ladder[i][j] = True
             if not flag:
                 # 게임 성공하면 count 반환
                 if start_game(n, board, _hor_ladder):
@@ -71,6 +75,6 @@ def solution(n, m, h, hor_ladder):
 
 
 n, m, h = map(int, input().split())
-hor_ladder = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+ladder = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
 
-print(solution(n, m, h, hor_ladder))
+print(solution(n, m, h, ladder))
