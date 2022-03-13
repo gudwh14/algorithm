232f81da421527171e33946645dd59ca4f3b1717
gudwh14 <alxious@naver.com>
2022-03-13 17:50:24 +0900

    boj : 2 * n 타일링

---
+++ boj/dp/2 * n 타일링.py
@@ -0,0 +1,13 @@
+def solution(n):
+    dp = [0 for _ in range(1001)]
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1]
+
+    return dp[n] % 10007
+
+
+n = int(input())
+print(solution(n))
